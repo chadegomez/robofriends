f33a170fb4b7eb1bdbd405161ff9613bcf61740d
Chad <chadegomez@yahoo.com>
2022-06-06 14:46:56 -0700

    Updates

---
+++ precache-manifest.02e4ad6b21ca61d9ebc0e2c8c8e89176.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "fa25e96ef5dd12704a86",
+    "url": "/robofriends/static/css/main.8174c30a.chunk.css"
+  },
+  {
+    "revision": "fa25e96ef5dd12704a86",
+    "url": "/robofriends/static/js/main.fa25e96e.chunk.js"
+  },
+  {
+    "revision": "709d58a229027795f766",
+    "url": "/robofriends/static/js/1.709d58a2.chunk.js"
+  },
+  {
+    "revision": "8262e6834e9a0ad46dd1",
+    "url": "/robofriends/static/css/2.19576000.chunk.css"
+  },
+  {
+    "revision": "8262e6834e9a0ad46dd1",
+    "url": "/robofriends/static/js/2.8262e683.chunk.js"
+  },
+  {
+    "revision": "b6cda56fffdec16fa127",
+    "url": "/robofriends/static/js/runtime~main.b6cda56f.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "4899a85d17c443aae9fd924c582a77a3",
+    "url": "/robofriends/index.html"
+  }
+];
